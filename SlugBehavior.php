4c43fc087a2608c1d97d06e8a0c7cd7615dd0b1c
Lartak <webmaster@lartak.fr>
2013-09-23 18:24:27 +0200

    Correction

--- SlugBehavior.php
+++ SlugBehavior.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 class SlugBehavior extends ModelBehavior{
 
 	public function setup(Model $Model, $settings = array()){
@@ -10,44 +10,25 @@ class SlugBehavior extends ModelBehavior{
 			);
 		}
 		$this->settings[$Model->alias] = array_merge(
-			$this->settings[$Model->alias], 
+			$this->settings[$Model->alias],
 			$settings
 		);
 	}
-
-	/*public function afterFind(Model $Model, $results, $primary = false){
-		$fieldSlug = $this->settings[$Model->alias]['fieldSlug'];
-		foreach($results as $k ->$result){
-			if(
-				isset($Model->data[$Model->alias]['id']) && 
-				isset($Model->data[$Model->alias][$fieldSlug])
-			){
-				$results[$k][$Model->data][$Model->alias]['url'] = array(
-					'controller'	=>	Inflector::tableize($Model->alias),
-					'action'		=>	'show',
-					'id'		=>	$Model->data[$Model->alias]['id'],
-					'slug'		=>	$Model->data[$Model->alias][$fieldSlug]
-				);
-			}
-		}
-		debug($results); die();
-		return $results;
-	}*/
 
 	public function beforeSave(Model $Model, $options = array()){
 		$fieldName = $this->settings[$Model->alias]['fieldName'];
 		$fieldSlug = $this->settings[$Model->alias]['fieldSlug'];
 		if(
-			isset($Model->data[$Model->alias][$fieldName]) && 
+			isset($Model->data[$Model->alias][$fieldName]) &&
 			(
-				!isset($Model->data[$Model->alias][$fieldSlug]) || 
-				empty($Model->data[$Model->alias][$fieldSlug]) || 
+				!isset($Model->data[$Model->alias][$fieldSlug]) ||
+				empty($Model->data[$Model->alias][$fieldSlug]) ||
 				$Model->data[$Model->alias][$fieldSlug] != strtolower(Inflector::slug($Model->data[$Model->alias][$fieldName]))
 			)
 		){
 			$Model->data[$Model->alias][$fieldSlug] = strtolower(
 				Inflector::slug(
-					$Model->data[$Model->alias][$fieldName], 
+					$Model->data[$Model->alias][$fieldName],
 					$this->settings[$Model->alias]['separator']
 				)
 			);
