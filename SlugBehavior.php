310c51c812d37a9d9c80a17dfc0c56a48e3f1797
Lartak <stephane.guichou@gmail.com>
2014-05-16 04:20:57 +0200

    Update SlugBehavior.php

--- SlugBehavior.php
+++ SlugBehavior.php
@@ -1,12 +1,13 @@
 <?php
-class SlugBehavior extends ModelBehavior{
+class SlugBehavior extends ModelBehavior {
 
-	public function setup(Model $Model, $settings = array()){
+	public function setup(Model $Model, $settings = array()) {
 		if(!isset($this->settings[$Model->alias])){
 			$this->settings[$Model->alias] = array(
 				'fieldName'	=>	'name',
 				'fieldSlug'	=>	'slug',
-				'separator'	=>	'-'
+				'separator'	=>	'-',
+				'lowercase'	=>	true
 			);
 		}
 		$this->settings[$Model->alias] = array_merge(
@@ -15,23 +16,26 @@ class SlugBehavior extends ModelBehavior{
 		);
 	}
 
-	public function beforeSave(Model $Model, $options = array()){
+	public function beforeSave(Model $Model, $options = array()) {
 		$fieldName = $this->settings[$Model->alias]['fieldName'];
 		$fieldSlug = $this->settings[$Model->alias]['fieldSlug'];
-		if(
+		$separator = $this->settings[$Model->alias]['separator'];
+		
+		if ($this->settings[$Model->alias]['lowercase'] == true) {
+			$slug = strtolower(Inflector::slug($Model->data[$Model->alias][$fieldName], $this->settings[$Model->alias]['separator']));
+		} else {
+			$slug = Inflector::slug($Model->data[$Model->alias][$fieldName], $this->settings[$Model->alias]['separator']);
+		}
+		
+		if (
 			isset($Model->data[$Model->alias][$fieldName]) &&
 			(
 				!isset($Model->data[$Model->alias][$fieldSlug]) ||
 				empty($Model->data[$Model->alias][$fieldSlug]) ||
-				$Model->data[$Model->alias][$fieldSlug] != strtolower(Inflector::slug($Model->data[$Model->alias][$fieldName]))
+				$Model->data[$Model->alias][$fieldSlug] != $slug
 			)
 		){
-			$Model->data[$Model->alias][$fieldSlug] = strtolower(
-				Inflector::slug(
-					$Model->data[$Model->alias][$fieldName],
-					$this->settings[$Model->alias]['separator']
-				)
-			);
+			$Model->data[$Model->alias][$fieldSlug] = $slug;
 		}
 	}
 }
